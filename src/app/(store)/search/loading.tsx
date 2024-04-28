2658553ecd2816b9757da7ee0e1bb223e5eab13b
Reginaldo <freestreetskate@gmail.com>
2024-04-28 02:39:07 -0300

    cypress

---
+++ src/app/(store)/search/loading.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { Skeleton } from "@/components/skeleton";
+import { useSearchParams } from "next/navigation";
+
+export default function SearchLoading() {
+    const searchParams = useSearchParams()
+
+    const query = searchParams.get('q')
+
+    return (
+        <div className="flex flex-col gap-4">
+            <p className="text-sm">
+                Resultados para: <span className="font-semibold">{query}</span>
+            </p>
+
+            <div>
+                <Skeleton className="h-[400px]" />
+                <Skeleton className="h-[400px]" />
+                <Skeleton className="h-[400px]" />
+                <Skeleton className="h-[400px]" />
+                <Skeleton className="h-[400px]" />
+                <Skeleton className="h-[400px]" />
+            </div>
+        </div>
+    )
+}
